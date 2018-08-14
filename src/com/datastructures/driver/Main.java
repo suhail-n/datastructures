481250222c1d79a62d26f9ee815b38bf0342a3d6
Suhail-n <suhail.nooristani@gmail.com>
2018-08-14 14:12:07 -0400

    using queue and stack

--- src/com/datastructures/driver/Main.java
+++ src/com/datastructures/driver/Main.java
@@ -1,12 +1,52 @@
 package com.datastructures.driver;
 
+
 import com.datastructures.linkedlist.LinkedList;
 import com.datastructures.linkedlist.Node;
+import com.datastructures.queue.Queue;
+import com.datastructures.stack.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		Queue<Integer> qu = new Queue<>();
+		System.out.println("Add 2");
+		qu.add(2);
+		System.out.println("Peek: " + qu.peek());
+		System.out.println("Add 3");
+		qu.add(3);
+		System.out.println("Peek: " + qu.peek());
+		System.out.println("Add 4");
+		qu.add(4);
+		System.out.println("Peek: " + qu.peek());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Add 4");
+		qu.add(4);
+		System.out.println("Peek: " + qu.peek());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		System.out.println("Remove: " + qu.remove());
+		
+		
+		System.exit(0);
+		Stack<Integer> st = new Stack<>();
+		System.out.println("Push: " + 2);
+		st.push(2);
+		System.out.println("Push: " + 33);
+		st.push(33);
+		System.out.println("Peek: " + st.peek());
+		System.out.println("Pop: " + st.pop());
+		System.out.println("Peek: " + st.peek());
+		System.out.println("Pop: " + st.pop());
+		System.out.println("Pop: " + st.pop());
+		
+		System.exit(0);
+		
 		LinkedList<Integer> ll = new LinkedList<>();
 		System.out.println("Appending");
 		ll.append(1);
@@ -33,6 +73,11 @@ public class Main {
 		for (Node<Integer> node : ll) {
 			System.out.println("Node=" + node.data);
 		}
+		System.out.println("----------------Recursion--------------------------");
+		System.out.println("----print nodes----");
+		ll.getHead().printNodes(ll.getHead());
+		System.out.println("---print reverse nodes---");
+		ll.getHead().reversePrintNodes(ll.getHead());
 
 	}
 }
